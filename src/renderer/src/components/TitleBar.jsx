8b33d17513b05a36f335c7fe73af9412d17c084f
PhuongNBM <phuongnbm-lab@github.com>
2026-05-31 00:46:40 +0700

    v260547

--- src/renderer/src/components/TitleBar.jsx
+++ src/renderer/src/components/TitleBar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useLang } from '../LangContext'
 
 const APP_TITLE = 'AVTotal Mixer'
-const APP_VERSION = 'v260546'
+const APP_VERSION = 'v260547'
 const GITHUB_REPO = 'phuongnbm-lab/avtotal-mixer'
 
 function daysLeft(dateStr) {
